0d1478739d84213c5bc319f4c7815260207d3bc9
Andy Tuddenham <andy@tudders.com>
2020-05-15 17:02:30 +0100

    Allow python code to produce more than one rom fle. Change output type of makefile from .bin to .rom. Update Makefile to (mostly) dynamicall work out what rom files to produce.

--- Makefile
+++ Makefile
@@ -1,29 +1,33 @@
-SRC_DIR = src
-BIN_DIR = bin
+SRC_DIR := src
+BIN_DIR := bin
 
-AS = ./vasm6502_oldstyle
-ASFLAGS = -Fbin -dotdir -wdc02
-DUMP = hexdump
-DUMPFLAGS = -C
-PY = python3
-BINFILES = rom.bin blink.bin HelloWorld.bin fibonacci.bin
-.SUFFIXES: .py .s .bin
+SRC := $(wildcard $(SRC_DIR)/*.s)
+ROMFILES := $(SRC:$(SRC_DIR)/%.s=$(BIN_DIR)/%.rom)
 
-all: $(BINFILES)
+AS := ./vasm6502_oldstyle
+ASFLAGS := -Fbin -dotdir -wdc02
+
+DUMP := hexdump
+DUMPFLAGS := -C
+
+all: $(ROMFILES) $(BIN_DIR)/blink_led.rom $(BIN_DIR)/rotate_led.rom
+
+$(BIN_DIR)/%.rom: $(SRC_DIR)/%.s | $(BIN_DIR)
+	$(AS) $(ASFLAGS) -o $@ $<
+	$(DUMP) $(DUMPFLAGS) $@
+
+$(BIN_DIR)/%.rom: $(SRC_DIR)/rom.py | $(BIN_DIR)
+	$(SRC_DIR)/rom.py -o$@ -c$*
+	$(DUMP) $(DUMPFLAGS) $@
 
 $(BIN_DIR):
 	mkdir $@
 
 install:
-#	minipro -p AT28C256 -w HelloWorld.bin
-	minipro -p AT28C256 -w fibonacci.bin
+#	minipro -p AT28C256 -w $(BIN_DIR)/HelloWorld.rom
+	minipro -p AT28C256 -w $(BIN_DIR)/fibonacci.rom
 
 clean:
-	-rm $(BIN_DIR)/*
+	$(RM) -r $(BIN_DIR)
 
-.s.bin:
-	$(AS) $(ASFLAGS) -o $(BIN_DIR)/$@ $(SRC_DIR)/$*.s
-	$(DUMP) $(DUMPFLAGS) $(BIN_DIR)/$@
-
-.py.bin:
-	$(PY) $< $(BIN_DIR)
+.PHONY: all clean
--- src/rom.py
+++ src/rom.py
@@ -1,21 +1,66 @@
-code = bytearray([
-    0xa9, 0xff,         # lda #$ff
-    0x8d, 0x02, 0x60,   # sta $6002
+#!/usr/bin/python3
 
-    0xa9, 0x55,         # lda #$55
-    0x8d, 0x00, 0x60,   # sta $6000
+import sys, getopt
 
-    0xa9, 0xaa,         # lda #$aa
-    0x8d, 0x00, 0x60,   # sta #6000
+def main(argv):
+    outfile = 'blink_led.rom'
+    codename = 'blink_led'
 
-    0x4c, 0x05, 0x80,   # jmp $8005
-    ])
+    blink_led = bytearray([
+        0xa9, 0xff,         # lda #$ff
+        0x8d, 0x02, 0x60,   # sta $6002
 
-rom = code + bytearray([0xea] * (32768 - len(code)))
+        0xa9, 0x55,         # lda #$55
+        0x8d, 0x00, 0x60,   # sta $6000
 
-rom[0x7ffc] = 0x00
-rom[0x7ffd] = 0x80
+        0xa9, 0xaa,         # lda #$aa
+        0x8d, 0x00, 0x60,   # sta #6000
 
-with open("rom.bin", "wb") as out_file:
-    out_file.write(rom)
+        0x4c, 0x05, 0x80,   # jmp $8005
+        ])
 
+    rotate_led = bytearray([
+        0xa9, 0xff,         # lda #$ff
+        0x8d, 0x02, 0x60,   # sta $6002
+
+        0xa9, 0x50,         # lda #$50
+        0x8d, 0x00, 0x60,   # sta $6000
+
+        0x6a,               # ror
+        0x8d, 0x00, 0x60,   # sta #6000
+
+        0x4c, 0x0a, 0x80,   # jmp $8005
+        ])
+
+    code = blink_led
+
+    try:
+        opts, args = getopt.getopt(argv,"ho:c:",["ofile=","code="])
+    except getopt.GetoptError:
+        print ('Usage rom.py -o <outputfile> -c <codename>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('rom.py -o <outputfile>')
+            sys.exit()
+        elif opt in ("-o", "--ofile"):
+            outfile = arg
+        elif opt in ("-c", "--code"):
+            codename = arg
+            if codename == 'rotate_led':
+                code = rotate_led
+            else:
+                code = blink_led
+    print ('Output file is ', outfile)
+    print ('Code is ', codename)
+
+    rom = code + bytearray([0xea] * (32768 - len(code)))
+
+    rom[0x7ffc] = 0x00
+    rom[0x7ffd] = 0x80
+
+    with open(outfile, "wb") as out_file:
+        out_file.write(rom)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
